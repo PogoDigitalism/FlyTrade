9ded7f7c86cd308757df995688457d7d761b8ae0
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2024-08-13 16:31:01 +0200

    Create SharedConstants.lua

---
+++ ReplicatedStorage/Trading/SharedConstants.lua
@@ -0,0 +1,10 @@
+local SharedConstants = {}
+
+SharedConstants.NEW_OFFER_ADDITION_TIME = 3
+SharedConstants.CONFIRMATION_WAIT_TIME = 5
+SharedConstants.COMPLETE_TRADE_TIME = 1
+
+SharedConstants.MAX_VALUE_DIFFERENCE = 100
+SharedConstants.MAX_OFFERS = 20
+
+return SharedConstants
